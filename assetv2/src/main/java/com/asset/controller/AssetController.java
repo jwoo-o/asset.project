9ad3b1bcf4ad0d4327e55311378b96ec1d507604
jwoh <dhwlsdn741@gmail.com>
2018-10-29 09:24:52 +0900

    차트 1차 수정

--- assetv2/src/main/java/com/asset/controller/AssetController.java
+++ assetv2/src/main/java/com/asset/controller/AssetController.java
@@ -30,9 +30,9 @@ import com.core.vo.ManagerDto;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class AssetController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	private static final Logger logger = LoggerFactory.getLogger(AssetController.class);
 
 	@Inject
 	private AssetService service;
