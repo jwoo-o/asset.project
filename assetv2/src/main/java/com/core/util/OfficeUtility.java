cce480b4c9b96ad9c82b27f45bf07fe24a998d2a
ghkt741 <ghkt741@K1076.gmkt.inc>
2019-03-12 18:10:42 +0900

    오피스 유틸 수정

--- assetv2/src/main/java/com/core/util/OfficeUtility.java
+++ assetv2/src/main/java/com/core/util/OfficeUtility.java
@@ -1,7 +1,5 @@
 package com.core.util;
 
-import java.util.regex.Pattern;
-
 import com.emp.vo.EmpVo;
 
 public class OfficeUtility {
@@ -9,12 +7,13 @@ public class OfficeUtility {
 	public static void input(EmpVo vo) throws Exception {
 		// TODO Auto-generated method stub
 		String seat = vo.getSeat();
-		Pattern p = Pattern.compile("^[n]");
-		if(p.equals(seat)) {
+
+		if(seat.contains("n3") || seat.contains("n13")) {
 			StringBuilder build = new StringBuilder(seat);
 			build.delete(seat.length()-4, seat.length());
 			vo.setOffice(build.toString());
 		}
 	}
+	
 
 }
