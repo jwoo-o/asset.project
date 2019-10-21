38f239f93142b75a35534f06c9d7b7f4ea7e3521
jinwoo <jinwoo@DESKTOP-6HTI579>
2019-10-21 21:39:34 +0900

    스케쥴러 생성

---
+++ assetv2/src/main/java/com/core/scheduler/Scheduler.java
@@ -0,0 +1,26 @@
+package com.core.scheduler;
+
+import javax.inject.Inject;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.core.controller.CoreController;
+import com.core.service.DeptService;
+
+@Component
+public class Scheduler extends CoreController{
+
+	@Inject
+	private DeptService service;
+	
+	@Scheduled(cron="0 0 * * 0")
+	public void orgChartScheduler() {
+		try {
+			service.selOrgChartDownload();
+		}catch (Exception e) {
+			// TODO: handle exception
+			logger.error(e.getMessage(),e);
+		}
+	}
+}
